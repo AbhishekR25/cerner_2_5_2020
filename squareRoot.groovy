b0b28dd3bb5c84baed3e884448e47ad3afe0e487
Ranjan <AR035595@Cerner.net>
2020-10-04 01:03:55 +0530

    Cerner 2_5_2020 Find Square root of a number in Groovy

---
+++ squareRoot.groovy
@@ -0,0 +1,10 @@
+/* Cerner_2^5_2020 */
+/* The following code determines the square root of a number */
+
+class Example { 
+   static void main(String[] args) { 
+      double value = 11.635;
+	  
+      System.out.printf("sqrt(%.3f) is %.3f%n", value, Math.sqrt(value)); 
+   } 
+}
