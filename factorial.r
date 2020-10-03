44885716a794632fd7b1e47dc603c1a7c115baca
Ranjan <AR035595@Cerner.net>
2020-10-04 01:12:27 +0530

    Cerner 2_5_2020 Find factorial of a number using R

---
+++ factorial.r
@@ -0,0 +1,5 @@
+#Cerner_2^5_2020
+# R program to calculate factorial value of given set of values using factorial() method  
+factorialValue <- factorial(c(0, 1, 2, 3, 4, 8))  
+  
+print(factorialValue)  
